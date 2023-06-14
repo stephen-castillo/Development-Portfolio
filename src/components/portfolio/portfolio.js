c11092ed8d6ec868c84934dfc20289ee18fa0f47
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-06-14 03:19:43 -0500

    Created mock projects and mapped them to the project component within the portfolio component

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,7 +1,32 @@
+import React from 'react';
+import './portfolio.css';
+import PROJECT from '../project/project';
+
 const PORTFOLIO = () => {
-    return (
-      <p>Portfolio</p>
-    );
-  }
-  
-  export default PORTFOLIO;
+
+  const projects = [
+    { id: 1, name: 'Project 1', description: 'Description 1' },
+    { id: 2, name: 'Project 2', description: 'Description 2' },
+    { id: 3, name: 'Project 3', description: 'Description 3' },
+    { id: 4, name: 'Project 4', description: 'Description 4' },
+    { id: 5, name: 'Project 5', description: 'Description 5' },
+    { id: 6, name: 'Project 6', description: 'Description 6' },
+  ];
+
+  return (
+    <div>
+       <h2>Portfolio</h2>
+      <div className="portfolio-grid">
+        {projects.map((project, index) => (
+          <PROJECT key={project.id} title={project.name}>
+            <p>{project.description}</p>
+            <p>Project #{index + 1}</p>
+            <p><a href="https://github.com">Project pages link</a></p>
+          </PROJECT>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default PORTFOLIO;
