0c3db75c88338363048e423c24cf487ab026bd50
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-06-15 00:25:54 -0500

    Figured out I needed the App.js file to control the imports and page load, reverted index.js to running App.js and added all the imports and page swapping functionality to App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
-import logo from './assets/images/logo.svg';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import HEADER from './components/header/header';
-import NAV from './components/header/navigation/nav';
 import ABOUT from './components/aboutme/aboutme';
-import PORFOLIO from './components/portfolio/portfolio';
+import PORTFOLIO from './components/portfolio/portfolio';
 import CONTACT from './components/contact/contact';
 import RESUME from './components/resume/resume';
 import FOOTER from './components/footer/footer';
 
 function App() {
-  return (
-    <div className="App">
-        
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <HEADER />
-        <NAV />
-        <ABOUT />
-        <PORFOLIO />
-        <CONTACT />
-        <RESUME />
-        <FOOTER />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentPage, setCurrentPage] = useState("About");
+
+    const RenderPage = () => {
+        switch (currentPage) {
+        case "About":
+        default:
+            return <ABOUT />;
+        case "Portfolio":
+            return <PORTFOLIO />;
+        case "Contact":
+            return <CONTACT />;
+        case "Resume":
+            return <RESUME />;
+        }
+    };
+
+    return (
+        <div className="App">
+            <HEADER setCurrentPage={setCurrentPage}/>
+                <RenderPage />
+            <FOOTER />
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import HEADER from './components/header/header';
-import ABOUT from './components/aboutme/aboutme';
-import PORTFOLIO from './components/portfolio/portfolio';
-import CONTACT from './components/contact/contact';
-import RESUME from './components/resume/resume';
-import FOOTER from './components/footer/footer';
+import App from './App';
+
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <HEADER />
-            <ABOUT />
-            <PORTFOLIO />
-            <CONTACT />
-            <RESUME />
-        <FOOTER />
+        <App />
     </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
